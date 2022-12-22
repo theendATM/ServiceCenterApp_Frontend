b52f199911e73b4ddfe422a08616fa4cfa018bfe
--theendATM <89829313824@mail.ru>
2022-12-23 00:23:58 +0500

    add nav highlight

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -7,12 +7,35 @@ import { useEffect } from 'react'
 
 const Navigation=()=>{
 
+ 
+
   const navigate = useNavigate();
 
   useEffect(()=>{
     let usersOption = document.querySelector('#usersOption');	
     (GetRole()!=='MainManager')?
     usersOption.classList.add('hidden'):usersOption.classList.remove('hidden');
+
+    var a =window.location.href;
+    let button='';
+    let buttons = document.querySelectorAll('.navButton');
+      buttons.forEach(b => {
+      b.classList.remove('highlight');
+    });
+
+    if(a.includes('warehouse'))
+      button = document.querySelector('.warehouseNavButton');
+    else if(a.includes('service'))
+      button = document.querySelector('.serviceNavButton');
+    else if(a.includes('catalogue'))
+      button = document.querySelector('.catalogueNavButton');
+    else if(a.includes('order'))
+      button = document.querySelector('.orderNavButton');
+    else if(a.includes('user'))
+      button = document.querySelector('.userNavButton');
+    if(button!=='')
+      button.classList.add('highlight') ;
+ 
   });
 
   return(
@@ -20,11 +43,11 @@ const Navigation=()=>{
       <div>
         <img className='logo' src={user} alt='' onClick={()=>navigate('/')}></img>
         <div className='act'>
-          <button onClick={()=>navigate('/services')}>Services</button>
-          <button onClick={()=>navigate('/warehouse')}>Local Warehouse</button>
-          <button onClick={()=>navigate('/catalogue')}>Catalogue Update</button>
-          <button onClick={()=>navigate('/orders/create')}>Orders</button>
-          <button id='usersOption' onClick={()=>navigate('/users')}>Users</button>
+          <button className='serviceNavButton navButton' onClick={()=>navigate('/services')}>Services</button>
+          <button className='warehouseNavButton navButton' onClick={()=>navigate('/warehouse')}>Local Warehouse</button>
+          <button className='catalogueNavButton navButton' onClick={()=>navigate('/catalogue')}>Catalogue Update</button>
+          <button className='orderNavButton navButton' onClick={()=>navigate('/orders/create')}>Orders</button>
+          <button className='userNavButton navButton' id='usersOption' onClick={()=>navigate('/users')}>Users</button>
         </div>
       </div>
       
--- src/components/Users/UserAdd.jsx
+++ src/components/Users/UserAdd.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router-dom'
 
 const UserAdd=()=>{
     const navigate = useNavigate();
-    
 
     return(
         <div className='catalogueUpdate usersManage'>
