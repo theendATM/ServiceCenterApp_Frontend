91ebf2009b7f378c482a7b29eb7b0ac8871095d3
BiassedTomato <43648634+BiassedTomato@users.noreply.github.com>
2022-12-22 20:47:11 +0500

    Fixed login and added base for role fetch

--- src/functions/GetRole.jsx
+++ src/functions/GetRole.jsx
@@ -1,9 +1,17 @@
-import { axios } from "axios";
+import axios from "axios";
 
-const GetRole=()=>
-{   
-    const role="MainManager"
-    return(role);
-}
+const GetRole = async () => {
+  try {
+    var results = await axios({
+      method: "get",
+      url: "https://localhost:7226/auth/roles",
+
+      withCredentials: true,
+    });
+    console.log(results.data)
+  } catch {}
+  const role = "MainManager";
+  return role;
+};
 
 export default GetRole;
--- src/pages/Authorization/SignInPage.jsx
+++ src/pages/Authorization/SignInPage.jsx
@@ -1,50 +1,58 @@
-import './SignInPage.css';
-import user from '../../components/img/def_user.png'
-import { useNavigate } from 'react-router-dom'
-import axios from 'axios';
-const SignInPage=() =>{ 
-    const navigate = useNavigate();
+import "./SignInPage.css";
+import user from "../../components/img/def_user.png";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
 
-    const logIn = async ({username,password})=>{
-        // const result=await axios({
-        //     method:"post",
-        //     url:"https://localhost:7226/auth/token",
-        //     data:{
-        //         Username:"Denis Zmeev",
-        //         Password:"123456"
-        //     }
-        //})
+const SignInPage = () => {
+  const [login, setLogin] = useState(undefined);
+  const [password, setPassword] = useState(undefined);
 
-        const result=await axios({
-                method:"get",
-                url:"https://localhost:7226/auth/roles",
-            })
+  const navigate=useNavigate()
 
-        
-        alert(result.data)
+  const logIn = async () => {
+    if (!password || !login) return;
+
+    try {
+      await axios({
+        method: "post",
+        url: "https://localhost:7226/auth/token",
+        data: {
+          Username: login,
+          Password: password,
+        },
+        withCredentials: true,
+      });
+
+      navigate("/")
+    } catch {
+      alert("wrong");
     }
+  };
 
-    return (
-      <div className='signInPage'>
-        <div className='logoAuth'>
-            <img src={user} alt=''></img>
-        </div>
-
-        <div className='signIn'>
-            <span>Sign in</span>
-            <div className='userData'>
-                <input type='email' placeholder='Email'></input>
-                <input type='password' placeholder='Password'></input>
-            </div>
-            <div className='buttons'>
-                <button onClick={()=>navigate('/register')}>Register</button>
-                <button onClick={logIn}>Sign in</button>
-            </div>
-        </div>
-
-
+  return (
+    <div className="signInPage">
+      <div className="logoAuth">
+        <img src={user} alt=""></img>
       </div>
-    )
-}
+
+      <div className="signIn">
+        <span>Sign in</span>
+        <div className="userData">
+          <input type="text" onChange={(event)=>setLogin(event.target.value)} placeholder="Login"></input>
+          <input
+            type="password"
+            onChange={(event)=>setPassword(event.target.value)}
+            placeholder="Password"
+          ></input>
+        </div>
+        <div className="buttons">
+          <button onClick={() => navigate("/register")}>Register</button>
+          <button onClick={logIn}>Sign in</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SignInPage;
