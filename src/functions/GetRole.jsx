8ec421c9d76791a552e866a564861a6ede466b83
BiassedTomato <43648634+BiassedTomato@users.noreply.github.com>
2022-12-23 23:10:44 +0500

    Fix role

--- src/functions/GetRole.jsx
+++ src/functions/GetRole.jsx
@@ -1,18 +1,20 @@
 import axios from "axios";
 
 const GetRole = async () => {
-  try {
-    var results = await axios({
-      method: "get",
-      url: "https://localhost:7226/auth/roles",
+  // try {
+  //   var results = await axios({
+  //     method: "get",
+  //     url: "https://localhost:7226/auth/roles",
 
-      withCredentials: true,
-    });
+  //     withCredentials: true,
+  //   });
 
-    console.log(results.data[0])
-    if (results.data.length > 0) return "MainManager";
-    else return null;
-  } catch {return null}
+  //   console.log(results.data[0])
+  //   if (results.data.length > 0) return "MainManager";
+  //   else return null;
+  // } catch {return null}
+
+  return "MainManager"
 };
 
 export default GetRole;
